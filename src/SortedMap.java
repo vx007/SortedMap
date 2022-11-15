b002d68e2193a020c1536bd6e2596c72b3afc80e
vx007 <vasilyev.x@mail.ru>
2022-11-15 14:29:25 +0300

    Homework

--- src/SortedMap.java
+++ src/SortedMap.java
@@ -1,4 +1,18 @@
+import java.util.HashMap;
+import java.util.Random;
+
 public class SortedMap {
+    public static HashMap<Integer, String> map= new HashMap<>(10);
+    public static Random random = new Random();
+
     public static void main(String[] args) {
+        for (int i =0 ; i< 10; i++){
+            map.put(i, intToString(random.nextInt()));
+        }
+        System.out.println(map);
+    }
+
+    public static String intToString(int i){
+        return String.format("%d", i);
     }
 }
